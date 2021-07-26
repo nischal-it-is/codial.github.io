80e98d3ba26c76443c8139f78e0b2afea997d8f8
Nischal Singh <singh.nischal.99@gmail.com>
2021-07-26 22:40:04 +0530

    setting up posts

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -32,6 +32,7 @@ module.exports.signIn = function(req, res){
 // get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
+        
         return res.redirect('back');
     }
 
